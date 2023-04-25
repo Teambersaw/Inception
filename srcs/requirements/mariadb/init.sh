63b3a38a71cc9e186150632c15ed3c9f094d4e4a
rossetti <jrossett@aol.fr>
2023-04-25 16:27:53 +0200

    pleure

--- srcs/requirements/mariadb/init.sh
+++ srcs/requirements/mariadb/init.sh
@@ -19,9 +19,6 @@ mariadb -e "CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE}";
 mariadb -e "CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_USER_PWD}'";
 mariadb -e "GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO '${MARIADB_USER}'@'%' WITH GRANT OPTION";
 mariadb -e "FLUSH PRIVILEGES";
-mariadb -e "SHOW DATABASES";
-mariadb -e "SHOW GRANTS FOR '${MARIADB_USER}'@'%'";
-mariadb -e "SELECT User FROM mysql.user";
 touch "$LOCK_FILE"
 /etc/init.d/mysql stop
 fi
