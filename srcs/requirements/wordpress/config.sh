53cebd6a4a8029f8ec603fc63f84940d38ca07f0
rossetti <jrossett@aol.fr>
2023-05-23 16:15:35 +0200

    oe

--- srcs/requirements/wordpress/config.sh
+++ srcs/requirements/wordpress/config.sh
@@ -10,7 +10,7 @@ wp core download --allow-root
 chown -R www-data:www-data /var/www/html/wordpress
 find /var/www/html/wordpress -type f -exec chmod 644 {} \;
 find /var/www/html/wordpress -type d -exec chmod 755 {} \;
-chmod 777 *
+chmod -R 777 *
 wp config create --dbname=wordpress --dbuser=wp --dbpass=wp --dbhost=mariadb:3306 --allow-root
 wp core install --url=jrossett.42.fr --title=inception --admin_user=wp --admin_password=wp --admin_email=jrossett@student.42.fr --skip-email --allow-root --path="/var/www/html/wordpress"
-exec /usr/sbin/php-fpm7.3 -F --allow-to-run-as-root
+php-fpm7.3 -RF
